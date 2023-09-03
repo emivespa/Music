86f58526692ecebd817d761157d70c0e4f2e60ea
Emilio Vesprini <emivespa1@gmail.com>
2023-09-03 21:09:11 +0000

    add flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "dev shell with yt-dlp from nixpkgs-unstable";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            yt-dlp
+          ];
+          shellHook =
+            ''
+              echo "+------------------------------------------------------+"
+              echo "| entering dev shell with yt-dlp from nixpkgs-unstable |"
+              echo "| if you get yt-dlp errors, delete flake.lock          |"
+              echo "+------------------------------------------------------+"
+            '';
+        };
+      });
+}
+
